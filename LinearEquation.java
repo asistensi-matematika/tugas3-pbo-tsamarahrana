f8e7085f85deca69eefa9b57033874219d6f33dd
tsamarahrana <55730503+tsamarahrana@users.noreply.github.com>
2019-10-09 14:18:33 +0700

    Add files via upload

---
+++ LinearEquation.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas3Asistensi;
+
+/**
+ *
+ * @author tsamarahrana
+ */
+public class LinearEquation {
+    
+}
